7d5a13a5b2d025a19230f17ee0433a6ce88fd6b8
David Smith <dave@davidsilvasmith.com>
2014-06-29 18:38:44 -0400

    updated requirements and gruntfile just 404 checks

--- README.md
+++ README.md
@@ -8,8 +8,11 @@ sowhatsbitcoin.com
 * LESS
   * Node
   * Grunt
-  * Watch-less
+  * gulp-less
 * Jekyll
+* Grunt for 404 checks
+* Gulp for spellehck, less, png crushing, etc.
+* Aspell http://docs.moodle.org/19/en/Configuring_aspell_on_Mac_OS_X
 
 # Images built with
 
@@ -17,6 +20,6 @@ sowhatsbitcoin.com
 * Inkscape
 * Favicons generated with http://realfavicongenerator.net/
 
-Facebook Debugger is useful for open graph
+Facebook Debugger is useful for open graph debugging
 
 https://developers.facebook.com/tools/debug/og/object?q=http%3A%2F%2Fwww.sowhatsbitcoin.com%2F%2Ffor-businesses%2F
--- gruntfile.js
+++ gruntfile.js
@@ -1,42 +1,5 @@
      module.exports = function(grunt) {
          grunt.initConfig({
-
-             less: {
-                dev: {
-                     options: {
-                         paths: ["assets/css"]
-                     },
-                     files: {"./css/sowhatsbitcoin.css": "./css/sowhatsbitcoin.less"}
-                 },
-                 production: {
-                     options: {
-                         paths: ["assets/css"],
-                         cleancss: true
-                     },
-                     files: {"./css/sowhatsbitcoin.css": "./css/sowhatsbitcoin.less"}
-                 }
-             },
-             watch: {
-                    files: ['**/*.less'],
-                    tasks: ['less:dev']
-             },
-             img : {
-                task1: {
-                src: './images'
-                }
-             },
-            spell: {
-//                     files: ['./_site/404.html']
-//                     files: ['./_site/About/index.html']
-//                     files: ['./_site/for-businesses/index.html']
-//                    files: ['./_site/for-developers/index.html']
-//                    files: ['./_site/for-individuals/buy/index.html']
-//                    files: ['./_site/for-individuals/faq/index.html']
-//                    files: ['./_site/for-individuals/index.html']
-                    files: ['./_site/index.html']
-
-                     //files: ['./_site/**/*.html']
-             },
          'link-checker': {
               dev: {
                 site: 'localhost',
@@ -45,24 +8,7 @@
                 }
               }
             },
-
-            filetransform: {
-                options: {
-                  // Task-specific options go here.
-                },
-                files: {
-                        'example': ['file1', 'file2']
-                },
-            },
          });
-         grunt.loadNpmTasks('grunt-contrib-less');
-         grunt.loadNpmTasks('grunt-contrib-watch');
-         grunt.loadNpmTasks('grunt-img');
-         grunt.loadNpmTasks('grunt-spell');
          grunt.loadNpmTasks('grunt-link-checker');
-         grunt.registerTask('default', ['watch']);
-         grunt.registerTask('check', ['spell']);
-         grunt.loadNpmTasks('grunt-filetransform');
-
-         grunt.registerTask('prod', ['img', 'less:production', 'link-checker' ])
+         grunt.registerTask('default', ['link-checker']);
      };
